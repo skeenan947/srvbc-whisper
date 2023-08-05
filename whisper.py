e031b35901ae27fa7e087c18fabfefc01e80df83
Shaun Keenan <skeenan@zencore.dev>
2023-08-05 16:37:26 -0700

    use argv

--- whisper.py
+++ whisper.py
@@ -1,15 +1,10 @@
 import os
+import sys
 
 import boto3
 s3 = boto3.client('s3')
 
-vidList = []
-objects = s3.list_objects(Bucket='srvbcvideo', Marker='r.mp4',Prefix='r')
-print("objects: {}".format(len(objects['Contents'])))
-for object in objects['Contents']:
-  if(len(object['Key']) == 12):
-    vidList.append(object['Key'])
-print(vidList)
+vidList = [sys.argv[1]]
 
 for vid in vidList:
   print('Decoding: {}'.format(vid))
@@ -17,9 +12,6 @@ for vid in vidList:
       "srvbcvideo", vid, vid
   )
   os.system("decipher transcribe -i {} --model large --language english".format(vid))
-  #dir = os.getcwd()
-  #transcribe(vid, language='english',model='large',output_dir='result',task='transcribe',subs=None)
-  #os.chdir(dir)
   s3 = boto3.client('s3')
   with open("result/{}".format(vid.replace('mp4','srt')), "rb") as f:
     s3.upload_fileobj(f, "srvbcvideo", "srt/{}".format(vid.replace('mp4','srt')))
