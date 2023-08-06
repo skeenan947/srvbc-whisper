7035e5d892021dfc4281248b665ac1023f0c5899
Shaun Keenan <skeenan@gmail.com>
2023-08-06 12:39:26 -0700

    use mp4 in name

--- whisper.py
+++ whisper.py
@@ -7,6 +7,7 @@ s3 = boto3.client('s3')
 vidList = [sys.argv[1]]
 
 for vid in vidList:
+  vid="{}.mp4".format(vid)
   print('Decoding: {}'.format(vid))
   s3.download_file(
       "srvbcvideo", vid, vid
