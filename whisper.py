06dabee68371c1fa2aa033e0a40902a5b9105b45
Shaun Keenan <skeenan@gmail.com>
2024-04-14 18:25:20 -0700

    switch to whisperx

--- whisper.py
+++ whisper.py
@@ -6,10 +6,12 @@ s3 = boto3.client('s3')
 
 vidList = sys.argv[1].split(',')
 
-for vid in vidList:
-  vid="{}.mp4".format(vid)
+for title in vidList:
+  wav="{}.wav".format(title)
+  vid="{}.mp4".format(title)
   print('Decoding: {}'.format(vid))
   s3.download_file(
       "srvbcvideo", vid, vid
   )
-  os.system("decipher transcribe -i {} --model large --language english".format(vid))
+  os.system("ffmpeg -i {} {}".format(vid, wav))
+  os.system("whisperx {} --model large-v2 -f srt --compute_type int8 --language en".format(vid))
